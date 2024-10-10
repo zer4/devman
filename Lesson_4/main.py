ab783fbcc866be4aeb6ab0a31e7a79638294edd3
Alexander Khudyakov <zer4@mail.ru>
2024-10-10 13:28:22 +0300

    First Commit

---
+++ Lesson_4/main.py
@@ -0,0 +1,35 @@
+from PIL import Image
+
+image = Image.open("monro.jpg")
+
+print("Ширина - ", image.width)
+print("Высота - ", image.height)
+print("Цветовая модель - ", image.mode)
+
+red, green, blue = image.split()
+
+# red channel
+coordinates1 = (50, 0, image.width, image.height)
+image1 = red.crop(coordinates1)
+
+coordinates2 = (25, 0, image.width-25, image.height)
+image2 = red.crop(coordinates2)
+
+image_red = Image.blend(image1, image2, 0.3)
+
+# blue channel
+coordinates3 = (0, 0, image.width-50, image.height)
+image3 = blue.crop(coordinates3)
+
+coordinates4 = (25, 0, image.width-25, image.height)
+image4 = blue.crop(coordinates4)
+
+image_blue = Image.blend(image3, image4, 0.3)
+
+# green channel
+coordinates5 = (25, 0, image.width-25, image.height)
+image_green = green.crop(coordinates5)
+
+final_image = Image.merge("RGB", (image_red, image_green, image_blue))
+final_image.thumbnail((80, 80))
+final_image.save("final_image.jpg")
